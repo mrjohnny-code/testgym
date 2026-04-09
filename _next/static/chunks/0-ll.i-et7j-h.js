ec5af1abf092b46b462a58562b69e130f2b07e35
ivan <vanek.94@list.ru>
2026-04-09 15:20:00 +0300

    Updates

--- _next/static/chunks/0-ll.i-et7j-h.js
+++
@@ -1,6 +0,0 @@
-__turbopack_load_page_chunks__("/_app", [
-  "static/chunks/0l0jqsn1yykc~.js",
-  "static/chunks/01fv~a56oc8au.js",
-  "static/chunks/0p4zmcy.yafpi.css",
-  "static/chunks/turbopack-0zc4d4tgwcx1d.js"
-])
--- _next/static/chunks/0swow1u12xtcp.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/0q55.-dihp4d_.js",
-  "static/chunks/01fv~a56oc8au.js",
-  "static/chunks/turbopack-0ixpt.jawusyf.js"
-])
