367e5fde50e58bf9ad030e55da4564be34a4f3ce
ivan <vanek.94@list.ru>
2026-04-09 15:16:53 +0300

    Updates

--- _next/static/chunks/0-ll.i-et7j-h.js
+++ _next/static/chunks/0-ll.i-et7j-h.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/0l0jqsn1yykc~.js",
   "static/chunks/01fv~a56oc8au.js",
-  "static/chunks/0qn4r4kj9hvcb.css",
-  "static/chunks/turbopack-0vz96dte9jozp.js"
+  "static/chunks/0p4zmcy.yafpi.css",
+  "static/chunks/turbopack-0zc4d4tgwcx1d.js"
 ])
